291ecf49ab93d9f011445bdb3ab7643b1215759d
unknown <bons6710hos@gmail.com>
2026-01-23 13:36:05 +0300

    added meneu and hours data to render

---
+++ chapter3/data/hoursdata.js
@@ -0,0 +1,17 @@
+export default{
+    defaultHours:{
+        opening:"09:00",
+        closing:"17:00"
+    },
+    Monday:{
+        opening:null,
+        closing:null
+    },
+
+    sunday:{
+        opening:"12:00",
+        closing:"16:00"
+
+    },
+
+}
