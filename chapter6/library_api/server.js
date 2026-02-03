0c7b8b50e7a041605773d5481d1202c238ea7fa4
bons <bons6710hos@gmail.com>
2026-02-03 15:20:38 +0300

    get api

--- chapter6/library_api/server.js
+++ chapter6/library_api/server.js
@@ -1,13 +1,25 @@
 import Fastify from 'fastify';
 
+import formbody from '@fastify/formbody';
+
 const fastify = Fastify();
+fastify.register(formbody);
+
 
 const port = 3000;
 
-fastify.get('/', async (request, reply) => {
-  return { message: 'Welcome to the Library API' };
-});
+// fastify.get('/', async (request, reply) => {
+//   return { message: 'Welcome to the Library API' };
+// });
+fastify.get('/', async (_request, reply) => {
+    reply.send({ message: 'Welcome to the Library API' });
+}); 
 
+fastify.setNotFoundHandler(async (request, reply) => {
+    const {mesage, statusCode} = request.error || {};
+    reply.status(statusCode || 500).send({
+      error: mesage || 'Resource not found'});
+});
 try{
     await fastify.listen({ port });
     console.log(`Server is running at http://localhost:${port}`);
@@ -16,4 +28,3 @@ try{
     console.error('Error starting server:', err);
     process.exit(1);
 }
-    
