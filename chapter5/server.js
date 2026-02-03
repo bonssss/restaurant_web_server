3d31178f0c2092da3ed90a14f99425a0ec1ec2a6
bons <bons6710hos@gmail.com>
2026-02-03 10:58:12 +0300

    added rss-parser package

--- chapter5/server.js
+++ chapter5/server.js
@@ -2,12 +2,16 @@ import Parser from "rss-parser";
 
 const parser = new Parser();
 
-const main = async()=>{
-    const url = "https://www.bonappetit.com/feed/recipes-rss-feed/rss";
-    const response = await fetch(url);
-    const data = await response.text();
-    const parsed = await parser.parseString(data);
-    console.log(parsed);
-}
+const main = async () => {
+  const url = "https://www.bonappetit.com/feed/recipes-rss-feed/rss";
+  // const response = await fetch(url);
+  // const data = await response.text();
+  // const parsed = await parser.parseString(data);
+  // console.log(parsed);
+  const { title, items } = await parser.parseURL(url);
+  console.log(`Feed Title: ${title}`);
+  const results = items.map(({ title, link }) => ({ title, link }));
+  console.log(results);
+};
 
 main();
