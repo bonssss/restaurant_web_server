87e738e69c9a5111b0187d9713b2468f20043f97
bons <bons6710hos@gmail.com>
2026-02-05 08:13:54 +0300

    feat: added full crud operations for book managment

--- chapter6/library_api/routes/booksRouter.js
+++ chapter6/library_api/routes/booksRouter.js
@@ -61,7 +61,91 @@ async function booksRouter(fastify, _opts) {
             reply.send(err)
         }
     });
+
+    fastify.get('/', async (request, reply) => {
+    try{
+        const books = await Book.findAll();
+        reply.send(books);
+    }catch(err){
+        console.error(err);
+        reply.send(err)
+    }
+});
+
+
+// update book
+
+fastify.put('/:id', async (request, reply) => {
+  const id = Number(request.params.id);
+  const { title, author } = request.body || {};
+
+  if (!title || !author) {
+    return reply.status(400).send({ error: 'Title and author are required' });
+  }
+
+  try {
+    const book = await Book.findByPk(id);
+
+    if (!book) {
+      return reply.status(404).send({ error: 'Book not found' });
+    }
+
+    book.title = title;
+    book.author = author;
+    await book.save();
+
+    return reply.send(book);
+  } catch (err) {
+    console.error(err);
+    return reply.status(500).send({ error: err.message });
+  }
+});
+
+//pathch book
+fastify.patch('/:id', async (request, reply) => {
+    const id = Number(request.params.id);
+    const { title, author } = request.body || {};
+    if (!title && !author) {
+        return reply.status(400).send({ error: 'At least one of title or author is required' });
+    }
+    try {
+        const book = await Book.findByPk(id);
+        if (!book) {
+            return reply.status(404).send({ error: 'Book not found' });
+        }
+
+        if (title) book.title = title;
+        if (author) book.author = author;
+        await book.save();
+        return reply.send(book);
+    } catch (err) {
+        console.error(err);
+        return reply.status(500).send({ error: err.message });
+    }
+});
+
+
+//delegete book
+
+    fastify.delete('/:id', async (request, reply) => {
+        const { id } = request.params;
+        try{
+            const book = await Book.findByPk(id); 
+            if(book){
+                await book.destroy();
+                reply.send({ message: 'Book deleted successfully' });
+            }else{
+                reply.status(404).send({ error: 'Book not found' });
+            }
+        }catch(err){
+            console.error(err);
+            reply.send(err)
+        }
+    });
+
     
 }
+
+
 
 export default booksRouter;
