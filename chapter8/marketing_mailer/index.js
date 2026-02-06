51b9fef07c1976d7d492fdc9a8e98cd27b4fa8c7
bons <bons6710hos@gmail.com>
2026-02-06 15:23:03 +0300

    added campaig email , email verification

--- chapter8/marketing_mailer/index.js
+++ chapter8/marketing_mailer/index.js
@@ -2,6 +2,8 @@ import fastify from "fastify";
 import formbody from "@fastify/formbody";
 import { sendMail } from "./services/mailer.js";
 import { welcomeMail } from "./mailTemplates.js";
+import { confirmationMail } from "./mailTemplates.js";
+import { campaignMail } from "./mailTemplates.js";
 import Lead from "./models/mailer.js";
 import dotenv from "dotenv";
 dotenv.config();
@@ -9,19 +11,60 @@ dotenv.config();
 
 const app = fastify();
 app.register(formbody);
+// const leads = await Lead.findAll();
+// console.log(leads);                                   
+
 
 app.post("/subscribe",  async (request, reply) => {
     const { email } = request.body;
     console.log(`New subscription reciver : ${email}`);
     try {
     await Lead.create({ email });
-    await sendMail(email, welcomeMail(email));
+    await sendMail(email, confirmationMail(email));
     reply.send({ message: "Subscription successful!" });
     } catch (e) {
         console.error(`Error processing subscription: ${e.message}`);
         reply.status(500).send({ error: "An error occurred while processing your subscription." });
     }
 });
+
+app.get("/verify/:email", async (request, reply) => {
+    const { email } = request.params;
+   
+    
+    console.log(`Verification request for : ${email}`);
+    try {
+        const lead = await Lead.findOne({ where: { email } });
+        if (!lead) {
+            reply.status(404).send({ error: "Lead not found." });
+            return;
+        }
+        lead.verified = true;
+        await lead.save();
+        reply.send({ message: "Email verified successfully!" });
+    }catch (e) {
+        console.error(`Error verifying email: ${e.message}`);
+        reply.status(500).send({ error: "An error occurred while verifying the email." });
+    }
+});
+
+app.get("/campaign/:campaignKey/user/:email/image.png", async (request, reply) => { 
+  const { email, campaignKey } = request.params; 
+  try {
+    const lead = await Lead.findOne({ where: { email } }); 
+    if (lead) {
+      lead.lastCampaign = campaignKey; 
+      await lead.save();
+      console.log(`${email} opened ${campaignKey}`); 
+    }
+  } catch (e) {
+    console.log("An error occurred", e.message);
+  }
+  reply.send({ message: "ok" }); 
+});
+
+
+sendMail("arifpay04@gmail.com", campaignMail("Special Promotion", "promo1", "arifpay04@gmail.com"))
 
 
 const port = process.env.PORT || 3000;
--- chapter8/marketing_mailer/mailTemplates.js
+++ chapter8/marketing_mailer/mailTemplates.js
@@ -10,3 +10,18 @@ export const welcomeMail = () => {
   const content = ` <h1>Welcome to Inn Box!</h1>`;
   return htmlTemplate(content);
 };
+
+export const confirmationMail = (url) => { 
+  const content = `<a href="${url}"><h1>Confirm your email</h1></a>`; 
+  return htmlTemplate(content);
+};
+
+
+export const campaignMail = (campaignText, campaignKey, email) => { 
+  const content = ` 
+  <h1>${campaignText}</h1>
+  <img src="http://localhost:4000/campaign/${campaignKey}/user/${email}/image.png"
+       style="display:none">
+  `;
+  return htmlTemplate(content);
+};
