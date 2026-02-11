6f27308144a8b97b68e0274f66165e4531d403bb
bons <bons6710hos@gmail.com>
2026-02-11 21:30:53 +0300

    modifacation added on env to add emails

--- chapter8/marketing_mailer/services/scheduler.js
+++ chapter8/marketing_mailer/services/scheduler.js
@@ -1,10 +1,12 @@
 import schedule, { scheduleJob } from "node-schedule";
+import dotenv from "dotenv";
+dotenv.config();
 import { sendMail } from "./mailer.js";
 import { campaignMail } from "../mailTemplates.js";
 export const schedules  = (timeOptions)=>{
    schedule.scheduleJob(timeOptions,async()=>{
         // console.log("Tme to send email");
-        sendMail("bons2112lidu@gmail.com", campaignMail("Special Promotion", "promo1", "bons2112lidu@gmail.com"));
+        sendMail(process.env.RECEIVER_EMAIL, campaignMail("Special Promotion", "promo1", process.env.SENDER_EMAIL));
         console.log("Email sent");
         
 
